bf2d861f958b28dbac3ac198c5c30f64f118b87f
nguyenduy7474 <nguyenduy7474@gmail.com>
2021-05-05 08:57:50 +0700

    v1-khong login

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/chatapp', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }); // connect to our database
+const db = mongoose.connection;
+const chatMessageSchema = new mongoose.Schema({
+    id: String,
+    users: Array,
+    createdAt: String,
+    messages: Array
+});
+const chatPersonSchema = new mongoose.Schema({
+    id: Number,
+    users: Array,
+    messages: Array
+});
+
+const chatMessage = mongoose.model('chatmessage', chatMessageSchema, 'chatmessage');
+const chatPerson = mongoose.model('persons', chatPersonSchema, 'persons');
+
+
+io.on("connection", socket => {
+    console.log("a user connected :D");
+
+    socket.on("chat message", obj => {
+        console.log(obj)
+        chatMessage.find({"id": obj.chatroomid}, (err, found) => {
+            if(err) throw err
+            console.log(found)
+            io.emit("chat message respone", found[0]);
+        })
+    });
+
+    socket.on("chat person", msg => {
+        chatPerson.find({}, (err, found) => {
+            if(err) throw err
+            io.emit("chat person respone", found)
+        })
+    });
+
+    socket.on("send message", obj => {
+        console.log(obj)
+        chatMessage.findOne({"id": obj.chatroomid}, (err, found) => {
+            if(err) throw err
+            countmsg = found.messages.length + 1
+            stmsg = "m" + countmsg
+            const event = new Date();
+            var objnewmsg = {
+                id: stmsg,
+                content: obj.message,
+                createdAt: event.toISOString(),
+                user: {
+                    id: obj.userid,
+                    name: obj.username
+                }
+            }
+            found.messages.push(objnewmsg)
+            found.save((err) => {
+                if(err) throw err
+                io.emit("chat message respone", found);
+            })
+        })
+    });
+
+
+});
+
+server.listen(port, () => console.log("server running on port:" + port));
